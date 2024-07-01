ce56ccde81d85c2dd1ee3cda34627504b10ec8aa
Brian Kipkorir <rotichbrian891@gmail.com>
2024-07-01 13:59:39 +0300

    lab completion

--- server/app.py
+++ server/app.py
@@ -21,6 +21,31 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        body = earthquake.to_dict()
+        status =  200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+     earthquake_list = []
+     for earthquake in Earthquake.query.filter(Earthquake.magnitude>=magnitude).all():
+         earthquake_list.append(earthquake.to_dict())
+     body = {'count': len(earthquake_list),
+             'quakes': earthquake_list
+             }    
+     status = 200
+     
+     return make_response(body, status)
+
 
 
 if __name__ == '__main__':
